fadb444da30e6d883ad23d73132cb44be1f0fc15
Sol_Ra <kimrayt61@gmail.com>
2024-08-27 11:40:56 +0600

    выполнил демоснтрационную задачу

--- src/main/java/kg/end_pont/gitpractice/controller/GitPracticeController.java
+++ src/main/java/kg/end_pont/gitpractice/controller/GitPracticeController.java
@@ -1,11 +1,17 @@
 package kg.end_pont.gitpractice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/git")
 public class GitPracticeController {
+
+    @GetMapping(value = "/hello")
+    public String sayHello(){
+        return "Hello Git Mega-Lab";
+    }
 
 
                         /*
