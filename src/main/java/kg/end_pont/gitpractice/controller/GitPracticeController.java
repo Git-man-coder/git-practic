4fc62e260ef0b14733e3b552c751b77da07f3776
Sol_Ra <kimrayt61@gmail.com>
2024-08-27 10:15:47 +0600

    создал репозиторий для практики студентов

---
+++ src/main/java/kg/end_pont/gitpractice/controller/GitPracticeController.java
@@ -0,0 +1,11 @@
+package kg.end_pont.gitpractice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/git")
+public class GitPracticeController {
+
+
+}
