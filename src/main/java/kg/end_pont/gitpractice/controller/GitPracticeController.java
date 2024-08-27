1e3e38b715214fab8458a753fd5bd4b4bac4bcf9
Zamir <50745794+zmrbk@users.noreply.github.com>
2024-08-27 11:41:55 +0600

    task_1 done (sum of two integers)

--- src/main/java/kg/end_pont/gitpractice/controller/GitPracticeController.java
+++ src/main/java/kg/end_pont/gitpractice/controller/GitPracticeController.java
@@ -1,11 +1,20 @@
 package kg.end_pont.gitpractice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/git")
 public class GitPracticeController {
-//asdasdas
+
+    @GetMapping(value = "/sum")
+    public String sum(
+            @RequestParam int a,
+            @RequestParam int b
+    ) {
+        return String.valueOf("Sum of " + a + " and " + b + " = " + (a + b));
+    }
 
 }
