8700f1dadf4dc7e6a1c02ac0cbe1356fdd4eeb1f
Sol_Ra <kimrayt61@gmail.com>
2024-08-27 10:56:25 +0600

    добавил деманстрационные измененния

--- src/main/java/kg/end_pont/gitpractice/controller/GitPracticeController.java
+++ src/main/java/kg/end_pont/gitpractice/controller/GitPracticeController.java
@@ -1,11 +1,16 @@
 package kg.end_pont.gitpractice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/git")
 public class GitPracticeController {
-
+    @GetMapping("/multiplication")
+    public String multiplication(@RequestParam int a, @RequestParam int b) {
+        return String.valueOf(a * b);
+    }
 
 }
