d43cedb0065cb692b76872db479fac1c0d92aaee
Sol_Ra <kimrayt61@gmail.com>
2024-08-27 10:56:25 +0600

    Изменил String в Integer. Сделал все требования

--- src/main/java/kg/end_pont/gitpractice/controller/GitPracticeController.java
+++ src/main/java/kg/end_pont/gitpractice/controller/GitPracticeController.java
@@ -1,11 +1,16 @@
 package kg.end_pont.gitpractice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/git")
 public class GitPracticeController {
-
+    @GetMapping("/multiplication")
+    public Integer multiplication(@RequestParam Integer a, @RequestParam Integer b) {
+        return a * b;
+    }
 
 }
